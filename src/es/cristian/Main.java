9acd21b1f2b255dcd30e18399a8c2c72a6d603bf
Cristian García Hernández <al036933@ieslaencanta.com>
2022-04-01 13:29:49 +0200

    cuadrado haciendo

---
+++ src/es/cristian/Main.java
@@ -0,0 +1,12 @@
+package es.cristian;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int numero = 0;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(" " + numero + " ");
+        }
+    }
+}
