f5519ef70a667aa59808114e2f96e42fd8f23170
Cristian García Hernández <al036933@ieslaencanta.com>
2022-04-13 09:50:12 +0200

    cuadrado haciendo

--- src/es/cristian/Main.java
+++ src/es/cristian/Main.java
@@ -2,10 +2,19 @@ package es.cristian;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < i + 1; j++) {
-                System.out.print("0 ");
-
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i == 0) {
+                    System.out.print("0 ");
+                } else if (i == 4) {
+                    System.out.print("0 ");
+                } else if (j == 0) {
+                    System.out.print("0 ");
+                } else if (j == 4) {
+                    System.out.print("0 ");
+                } else {
+                    System.out.print("  ");
+                }
             }
             System.out.println();
         }
